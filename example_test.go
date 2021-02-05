bb3bb20a7e3e1fe52cbdc7ff1b5e3b48684830c2
alanmkt <alan.saputra@klikdokter.com>
2021-02-05 19:47:38 +0700

    rename package

--- example_test.go
+++ example_test.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"regexp"
 
-	"github.com/go-ozzo/ozzo-validation/v4"
-	"github.com/go-ozzo/ozzo-validation/v4/is"
+	"github.com/itgelo/ozzo-validation"
+	"github.com/itgelo/ozzo-validation/is"
 )
 
 type Address struct {
--- is/rules_test.go
+++ is/rules_test.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/itgelo/ozzo-validation"
 	"github.com/stretchr/testify/assert"
 )
 
